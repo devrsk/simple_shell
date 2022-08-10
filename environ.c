954147c2ec06120c76dbcf710855a13882da60a6
Raheem Salifu Kasim <r.kasim@aol.com>
2022-08-10 14:35:24 +0000

    Task Update

--- environ.c
+++ environ.c
@@ -1,20 +1,70 @@
 #include "shell.h"
-/**
- * printenv - prints the environment variable
- * @env: the environment variable to print
- * @ex_st: exit status
- * Return: Always (0) for sucess
- */
-int printenv(char **env, int *ex_st)
-{
-	unsigned int i = 0;
 
-	while (env[i] != NULL)
+/**
+ * get_enviroment - function that gets PATH from environment
+ * @env: enviroment
+ * @comd: command to add
+ * Return: resv_concat
+ */
+char *get_enviroment(char **env, char *comd)
+{
+	int i, c, len = 0;
+	char **new_env = NULL;
+	char array_path[5];
+	char *path_char, *resv_concat;
+
+	for (i = 0; env[i] != NULL; i++)
 	{
-		print_str(env[i]);
-		_putchar('\n');
-		i++;
+		for (c = 0; c < 4; c++)
+			array_path[c] = env[i][c];
+
+		array_path[c] = '\0';
+
+		if (_strcmp(array_path, "PATH") == 0)
+		{
+			for (c = c + 1; env[i][c] != '\0'; c++)
+				len++;
+			path_char = malloc(sizeof(char) * len + 1);
+			if (path_char == NULL)
+				return (NULL);
+
+			for (c = 5; env[i][c] != '\0'; c++)
+				path_char[c - 5] = env[i][c];
+
+			path_char[c - 5] = '\0';
+			break;
+		}
 	}
-	*ex_st = 0;
-	return (EXIT_SUCCESS);
+
+	new_env = brokentoken(path_char, ":");
+	resv_concat = get_dir_concat(new_env, comd);
+	free(path_char);
+	return (resv_concat);
+}
+
+/**
+ * get_dir_concat - function adding command
+ * @new_env: directory array
+ * @comd: command to add
+ * Return: new_env
+ */
+char *get_dir_concat(char **new_env, char *comd)
+{
+	char *dir_concat;
+	int i;
+
+	for (i = 0; new_env[i] != NULL; i++)
+	{
+		dir_concat = _strdup(new_env[i]);
+		dir_concat = _strcat(dir_concat, "/");
+		dir_concat = _strcat(dir_concat, comd);
+		if (access(dir_concat, X_OK) == 0)
+		{
+			free(new_env);
+			return (dir_concat);
+		}
+		free(dir_concat);
+	}
+	free(new_env);
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,45 +1,91 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define TRUE (1 == 1)
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
 
-/* path */
-int _path(char *first, char **input, char **env, int *ex_st);
+/**
+ * struct var_input - struct
+ * @count: count
+ * @buffer: buffer
+ * @array_inputs: array_inputs
+ * @name_pro: name_pro
+ * @exitstatus:exitstatus
+ * typedef input_v: new name struct
+ */
+typedef struct var_input
+{
+	int count;
+	char *buffer;
+	char **array_inputs;
+	char *name_pro;
+	int exitstatus;
+} input_v;
 
-/* environment */
-int printenv(char **env, int *ex_st);
-char *cpy_env(char **env);
+/**
+ * struct com_struct - struct
+ * @name: name
+ * @p: p
+ * typedef comand_v: new name struct
+ */
+typedef struct com_struct
+{
+	char *name;
+	int (*p)(input_v *, char **);
+} comand_v;
 
-/* built-ins & execute function */
-void builtins(char *line, char **args, char **env, int *ex_st);
-void _execute(int status, char **args, int *ex_st, int *tal);
+/**
+ * struct com_help - struct
+ * @name: name
+ * @p: p
+ * typedef input_v: new name struct
+ */
+typedef struct com_help
+{
+	char *name;
+	char (*p)(input_v *);
+} help_v;
 
-/* strings.c: functions related to strings */
-int _strlen(char *s);
-char *_strdup(char *str);
+/*utility functions*/
+int check_for_comand(input_v *vars, char **env);
+char **brokentoken(char *buffer, char *delimiter);
+int comd_handling(input_v *vars, char **env);
+char *get_enviroment(char **env, char *comd);
+char *get_dir_concat(char **new_env, char *comd);
+int take_env(input_v *vars, char **env);
+
+
+/*methods function C*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strtok(char *line, char *delim);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *convers_integer(int count);
+size_t _getline(char **buff);
 
-/* printf.c: functions related to printing strings/characters/numbers */
-int _putchar(char c);
-void print_str(char *s);
-char *pathstr(char *right, char *first);
-char **parser(char *l);
-void print_int(int *tal);
+/*Command functions*/
+int exit_func(input_v *vars, char **env);
 
-
-/* helpers.c: helper functions */
-void no_nl(char *l);
-void free_grid(char **grid, int height);
-int special_char(char *buffer, ssize_t bytes, int *ex_st);
+/*funtion Help*/
+char *read_file(char *namefile);
+int help_func(input_v *vars, char **env);
+char (*help_match(input_v *vars))(input_v *vars);
+char print_exit(input_v *vars);
+char print_pwd(input_v *vars);
+char print_cd(input_v *vars);
+char print_help(input_v *vars);
 
 #endif
