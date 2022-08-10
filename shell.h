e0c4a8e8eaeb5a2da60ac3a6e8a9f0012062dcda
Raheem Salifu Kasim <r.kasim@aol.com>
2022-08-10 12:08:24 +0000

    Group Task Update

--- shell.h
+++ shell.h
@@ -1,58 +1,144 @@
+#ifndef SHELLH
+#define SHELLH
 
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
-/*libraries*/
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <signal.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
+#include "history.h"
+#include "shellvars.h"
+/*#include <string.h>*/
 
-/*main.c*/
-void INThandler(int sig);
-void print_dollar(void);
+/* from in.c */
+int shintmode(void);
 
-/*find_command.c*/
-unsigned int find_length_command(char *s);
-char **array_strtok(char *str);
+/* from _printenv.c */
+int _printenv(void);
 
-/*execute.c*/
-void execute(char **commands, char *buffer, char **env,
-		char **argv, int count);
+/* from cmdcall.c */
+int builtincall(char *av[]);
+int cmdcall(char *av[], char *path);
 
-/*function_for_strings.c*/
-char *_strncpy(char *dest, char *src, int n);
-char *_strncpyconst(char *dest, const char *src, int n);
-unsigned int _strlen_const(const char *str);
-unsigned int _strlen(char *str);
-int _strcmp(char *s1, char *s2);
+/* from parser.c */
+int parseargs(char **buf);
 
-/*env.c*/
-unsigned int find_num_dir(char *path);
-char **store_e_variables(char *fir_com, char **environ);
-char *_getenv(const char *name, char **environ);
-char *_strncpcommand(char *dest, char *src, char *command, int n, int c);
-void print_env(char **environ);
+/* from errhandl.c */
+int errhandl(int status);
 
-/*free_all.c*/
-void free_all_dp(char **ptr);
-void parent_free_commands(char *buffer, char **commands);
-void send_to_free(char *buffer, char **commands);
+/* from string.c */
+size_t _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *, char *);
+char *_strdup(char *str);
+char *_strcat(char *a, char *b);
 
-/*error_messages.c*/
-void build_message(char **av, char *fir_com, int count);
-int _puterror(char c);
-void end_file(char *buffer);
-void fork_fail(void);
+/* from _getenv.c and getenviron.c */
+char ***getenviron(void);
+int setallenv(char **environ, char *add);
+char *_getenv(char *avzero);
+int _setenv(char *name, char *val);
+int _unsetenv(char *name);
+char **getallenv(void);
 
-/*child_process.c*/
-void null_command(char *buffer);
-void get_out(char *buffer, char **commands);
-void env_end(char *buffer, char **commands, char **env);
-void _path(char **commands, char *buffer, char **env, char **argv, int count);
+
+/* from utility.c */
+char *itos(int digits);
+char *_strchr(char *s, char c);
+int fprintstrs(int fd, char *str, ...);
+int printerr(char *);
+int linecount(int);
+
+/* from cd.c */
+int _cd(char *av[]);
+
+/* from alias.c */
+int aliascmd(char **av);
+char *getalias(char *name);
+int unsetalias(char *name);
+
+/* from shellvars.c */
+int initsvars(int ac, char **av);
+char *getsvar(char *name);
+int setsvar(char *name, char *val);
+int unsetsvar(char *name);
+ShellVar **getspecial(void);
+ShellVar **getvars(void);
+
+/* from _realloc.c */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* from _strtok.c */
+char *strtok(char *str, char *delim);
+
+/* from _getline.c */
+int _getline(char **lineptr, int fd);
+
+char *strtokqe(char *str, char *delim, int escflags);
+
+/*from history.c*/
+int sethist(char *cmd);
+int print_hist(void);
+int exit_hist(void);
+
+/* from _printenv.c */
+int _printenv(void);
+int _putchar(char c);
+
+/*from help.c*/
+int help(char *cmd);
+
+/* from exitcleanup.c */
+void exitcleanup(char **av);
+
+/* from _atoi*/
+int _atoi(char *s);
+
+char *_getpid(void);
+
+/**
+ * struct ShellVar - shellvar list node
+ * @name: name of variable
+ * @val: value of variable
+ * @next: next variable in the list
+ */
+typedef struct ShellVar
+{
+	char *name;
+	char *val;
+	struct ShellVar *next;
+} ShellVar;
+
+/**
+ * struct HistList - singly linked list
+ * @cmd: command line entered
+ * @next: pointer to the next node
+ * Description: singly linked list node structure for History 
+ * for Holberton project
+ */
+
+typedef struct HistList
+{
+	char *cmd;
+	struct HistList *next;
+} HistList;
+
+/**
+ * struct AliasData - struct for alias function
+ * @AliasData - name of the struct
+ * @name: name of the alias value
+ * @val: value for the alias
+ * @next: pointer to next node
+ *
+ */
+
+typedef struct AliasData
+{
+	char *name;
+	char *val;
+	struct AliasData *next;
+} AliasData;
 
 #endif
