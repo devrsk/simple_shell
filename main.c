e62a63670931e948bd2961e5b57cf2b502d63f0c
Raheem Salifu Kasim <r.kasim@aol.com>
2022-08-10 19:32:27 +0000

    Task Update

--- main.c
+++ main.c
@@ -13,58 +13,6 @@ void handle_ctrld(input_v *vars, int len)
 		exit(0);
 	}
 }
-
-/**
- * main - Main arguments functions
- * @ac: Count of argumnents
- * @av: Arguments
- * @env: Environment
- * Return: _exit = 0.
- */
-int shell(int ac, char **av, char **env)
-{
-	int pathValue = 0, status = 0, is_path = 0;
-	char *line = NULL, /**ptr to inpt*/ **commands = NULL; /**tokenized commands*/
-	(void)ac;
-	while (1)/* loop until exit */
-	{
-		errno = 0;
-		line = _getline_command();/** reads user input*/
-		if (line == NULL && errno == 0)
-			return (0);
-		if (line)
-		{
-			pathValue++;
-			commands = tokenize(line);/** tokenizes or parse user input*/
-			if (!commands)
-				free(line);
-			if (!_strcmp(commands[0], "env"))/**checks if user wrote env"*/
-				_getenv(env);
-			else
-			{
-				is_path = _values_path(&commands[0], env);/** tokenizes PATH*/
-				status = _fork_fun(commands, av, env, line, pathValue, is_path);
-					if (status == 200)
-					{
-						free(line);
-						return (0);
-					}
-				if (is_path == 0)
-					free(commands[0]);
-			}
-			free(commands); /*free up memory*/
-		}
-		else
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);/** Writes to standard output*/
-			exit(status);
-		}
-		free(line);
-	}
-	return (status);
-}
-
 /**
  * main - function that evaluates whether the command exists
  * @ac: count
@@ -74,7 +22,6 @@ int shell(int ac, char **av, char **env)
  */
 int main(int ac, char **av, char **env)
 {
-	int shell(int ac, char **av, char **env);
 	input_v var = {0, NULL, NULL, NULL, 0};
 	size_t cant_buff = 0;
 	ssize_t len = 0;
