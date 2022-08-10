0c5b6185fd85c60605028a1d08ef6d98b3c69560
Raheem Salifu Kasim <r.kasim@aol.com>
2022-08-10 19:29:13 +0000

    Task Update

--- shell.h
+++ shell.h
@@ -88,4 +88,22 @@ char print_pwd(input_v *vars);
 char print_cd(input_v *vars);
 char print_help(input_v *vars);
 
+int lsh_ctrld(char **args);
+int lsh_cd(char **args);
+int lsh_help(char **args);
+extern char **environ;
+int lsh_exit(char **args);
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _putchar(char c);
+
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
+char *_getline_command(void);
+void _getenv(char **env);
+char **tokenize(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
+char *lineptr, int np, int c);
+
 #endif
